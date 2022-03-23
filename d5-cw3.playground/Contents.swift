4ca4bb74ae5e056838dc6827312e3acba0338728
Mo74mm3d <mohammedjalqattan@icloud.com>
2022-03-23 16:45:02 +0300

    Done

--- d5-cw3.playground/Contents.swift
+++ d5-cw3.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func printUserName(){
+    print("My name is Mohammed Alqattan")
+}
+
+printUserName()
+
+func grade(first: Int, second: Int, final: Int) -> String{
+    let total = final + first + second / 3
+    if (total >= 90){
+        return "A"
+    } else if (total >= 80){
+        return "B"
+    } else if (total >= 70){
+        return "C"
+    } else if (total >= 60){
+        return "D"
+    } else{
+        return "F"
+    }
+}
